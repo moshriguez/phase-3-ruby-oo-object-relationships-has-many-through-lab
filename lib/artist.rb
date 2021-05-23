99359e0d1e074d2f12294ccd542922b214a7e402
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-23 16:10:03 -0700

    completed

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,29 @@
+class Artist
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def genres
+        self.songs.map {|song| song.genre}
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.patient == self
+        end
+    end
+
+    def doctors
+        self.appointments.map {|appt| appt.doctor}
+    end
+end
