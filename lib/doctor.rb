240629380a6828664f949054d8d875259f5b521c
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-23 16:09:58 -0700

    commit tree 6cfff3eff5f9029ae0144c6bcb612680b05c5c74

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,29 @@
+class Doctor
 
+    @@all = []
+    
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.doctor == self
+        end
+    end
+
+    def patients
+        self.appointments.map {|appt| appt.patient}
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,25 @@
+class Genre
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.genre == self
+        end
+    end
+
+    def artists
+        self.songs.map {|song| song.artist}
+    end
+end
